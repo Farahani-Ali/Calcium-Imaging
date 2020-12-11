0a5a3d90e758874b5ff2fc9700439227d9042ef7
Farahani-Ali <farahani7ali@gmail.com>
2020-12-11 17:36:37 -0600

    remove the vevn folder

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from ActiveTime import ActiveTime
 from Spikes import Spikes
 
+import numpy as np
 
 dataSetPath = "./Datasets/F_Plus_Glue.csv"
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calcium-Imaging
+
+There are data sets of calium igaming treatment of neurons
